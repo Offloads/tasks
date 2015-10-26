8cc0bdc7b3a6bbbef6da4537824e1d992e1b2c11
Nik <nikodonnell@gmail.com>
2015-10-26 20:03:09 +0100

    update of api app

---
+++ app.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    // ajax!!! 
+ 
+    $(".form").submit(function (event) {
+        event.preventDefault();
+       $( ".weather" ).empty(); 
+        var city = $("#city").val();
+        console.log(city);
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=81c2ea79fca364daa87277ef91a150b3").done(
+                        function (data){
+                        console.log(data);
+                        
+                        $("div.weather").append(data.weather[0].description);
+$("div.temp").append(data.main.temp - 273.15 + "℃"
+                    );
+$("div.info").append(data.main.humidity + "%");
+                  
+        });
+         
+        $("h2.weather-city").text(city);
+        $("#city").val("");
+    
+    });
+
+
+    
+    
+});

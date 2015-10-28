909a7de2c64ca4102c71f18b002e6f721746fed4
Nik <nikodonnell@gmail.com>
2015-10-28 12:12:30 +0100

    few changes to css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // ajax!!! 
  /*$.ajax({
  
-    url:"",
+    url:"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=81c2ea79fca364daa87277ef91a150b3",
     
     data: {
      id:
@@ -17,7 +17,8 @@ $(document).ready(function () {
         $( " ").html( json.html ).appendTo( "body" );
     },
  });
- */    
+ */ 
+
     $(".form").submit(function (event) {
         event.preventDefault();
        $( ".weather" ).empty();
@@ -25,14 +26,14 @@ $(document).ready(function () {
         $( ".info" ).empty();
         var city = $("#city").val();
         console.log(city);
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=81c2ea79fca364daa87277ef91a150b3").done(
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=81c2ea79fca364daa87277ef91a150b3").done(
                         function (data){
                         console.log(data);
                         
-                        $("div.weather").append(data.weather[0].description);
-$("div.temp").append(data.main.temp + "℃"
-                    );
+$("div.weather").append(data.weather[0].description);
+$("div.temp").append(data.main.temp + "℃");
 $("div.info").append(data.main.humidity + "%");
+$("div.weather").append(data.weather.rain);
                   
         });
          
